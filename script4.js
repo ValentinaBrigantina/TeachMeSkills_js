892aaa2d9696351b40ab3cde5469ecc6cd5c6cd1
Valentina <matsulevichvalentina@gmail.com>
2021-12-04 12:05:17 +0300

    removed brake from while

--- script4.js
+++ script4.js
@@ -2,13 +2,14 @@
 
 let expectedColor = prompt('Загадай цвет!');
 let attemptNumber = Number(prompt('Сколько тебе нужно попыток, чтобы угадать цвет?', 3));
+let color;
 
-while (attemptNumber > 0) {
-    let color = prompt('Угадывай!');
+while (attemptNumber > 0 && expectedColor !== color) {
+    color = prompt('Угадывай!');
     attemptNumber -= 1;
 
-    if (expectedColor == color ) {
-        alert('Поздравляю, угадал!'); break;
+    if (expectedColor == color) {
+        alert('Поздравляю, угадал!');
     }
     else if (attemptNumber == 0) {
         alert('К сожалению, попытки закончились');
